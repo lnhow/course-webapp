aebf87b5cd1ce0a6a345fe966d9cd7c91c881eff
NhatHiepisme <hiepsimattroi1807@gmail.com>
2021-01-01 11:10:48 +0700

    Hash password

--- routes/account/account.route.js
+++ routes/account/account.route.js
@@ -1,4 +1,9 @@
-const router = require('express').Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const monent =  require('moment');
+
+const userModel = require('../../models/user.model')
+const router = express.Router();
 
 module.exports = router;
 
@@ -6,4 +11,18 @@ router.get('/register', async function (req, res){
     res.render('vwAccount/register');
 })
 
+router.post('/register', async function (req, res){
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    const user = {
+        username: res.body.username,
+        password: hash,
+        dob: dob,
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    await userModel.add(user);
+    res.render('vwAccount/register');
+})
 module.exports = router;
