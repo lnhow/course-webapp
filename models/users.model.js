537f0fb2d108066d7eff5a98f58701744590ba68
bachtangthang <thang5217113@gmail.com>
2021-01-02 15:37:28 +0700

    Update users.model.js
    hash and compare password

--- models/users.model.js
+++ models/users.model.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 //not done
+const bcrypt = require('bcrypt-nodejs');
 const userSchema = new mongoose.schemna({
     username: {type: String, required : true},
-    password: {type: String, required : true}
+    password: {type: String, required : true},
+    email: String
 });
 
-const User = mongoose.model('user', userSchema)
+userSchema.methods.encryptPassword = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+
+const User = mongoose.model('user', userSchema);
 module.exports = User
